ce645918a963b73f65945c7407c78770b836104b
josh <jvcoding9407@gmail.com>
2024-09-18 19:17:49 -0700

    Add mongoose to project and create schemas for launches and planets

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: { type: Number, required: true },
+  mission: { type: String, required: true },
+  rocket: { type: String, required: true },
+  launchDate: { type: Date, required: true },
+  target: { type: String, required: true },
+  customers: [String],
+  upcoming: { type: Boolean, default: true, required: true },
+  success: { type: Boolean, default: true, required: true },
+});
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasaAdmin:4hhYyfbv17C8gSCC@nasa-data.djydk.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASA-Data";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
