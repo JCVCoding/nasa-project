4cbadac8703c5431ff8d8f099336289e9ef4f9a8
josh <jvcoding9407@gmail.com>
2024-09-20 12:15:53 -0700

    Sort the paginated data and add pagination to launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -88,7 +88,11 @@ async function getLatestFlightNumber() {
 }
 
 async function getAllLaunches(skip, limit) {
-  return await launches.find({}, { __id: 0, __v: 0 }).skip(skip).limit(limit);
+  return await launches
+    .find({}, { __id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
